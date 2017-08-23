f1a842c76774883dceefbc2fdc13b4116849b1b8
wei33107@gmail.com <wei33107@gmail.com>
2017-08-23 12:29:55 +0800

    2017082301

---
+++ futureBacken/src/main/java/com/futureBacken/FutureBackenApplication.java
@@ -0,0 +1,12 @@
+package com.futureBacken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureBackenApplication.class, args);
+	}
+}
